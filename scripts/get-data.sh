50b30373fd5bb4213445a2c803cd9beccf665f3e
StaGlo <stanley.glowczewski@gmail.com>
2025-05-28 22:35:36 +0200

    Split preparing and streaming data into two scripts

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -9,6 +9,7 @@ CRIMES_URL="https://www.cs.put.poznan.pl/kjankiewicz/bigdata/stream_project/crim
 DOWNLOAD_DIR="/tmp/crime_data"
 
 # Kafka settings
+CLUSTER_NAME=$(/usr/share/google/get_metadata_value attributes/dataproc-cluster-name)
 BOOTSTRAP_SERVER="${CLUSTER_NAME}-m:9092"
 TOPIC_NAME="crimes-in-chicago-topic"
 
@@ -27,24 +28,3 @@ wget -q "${CRIMES_URL}" -O "${DOWNLOAD_DIR}/crimes.zip"
 # --- Unpack crimes data ---
 echo "$(date '+%Y-%m-%d %H:%M:%S') Unzipping crimes data"
 unzip -q "${DOWNLOAD_DIR}/crimes.zip" -d "${DOWNLOAD_DIR}/crimes"
-
-# --- Stream data into Kafka ---
-echo "$(date '+%Y-%m-%d %H:%M:%S') Starting to produce records to Kafka topic '${TOPIC_NAME}'"
-for csvfile in "${DOWNLOAD_DIR}/crimes"/*.csv; do
-    echo "  Processing file: ${csvfile}"
-    # Skip header line, then send each line as a JSON-like CSV string
-    tail -n +2 "${csvfile}" | while IFS= read -r line; do
-        echo "$line" | kafka-console-producer.sh \
-            --broker-list "${BOOTSTRAP_SERVER}" \
-            --topic "${TOPIC_NAME}" \
-            --property "parse.key=false" \
-            --property "parse.headers=false" \
-            --property "key.separator=:"
-
-        # Sleep
-        sleep 1
-
-    done
-done
-
-echo "$(date '+%Y-%m-%d %H:%M:%S') Finished producing all records."
